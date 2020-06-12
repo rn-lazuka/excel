3ed41195642fdd40946ddb79429f2babd41bb171
rn-lazuka <58468458+rn-lazuka@users.noreply.github.com>
2020-06-12 19:13:04 +0300

    Framework start (#4)
    * finish base framework functional
    
    * comment debug
    
    Co-authored-by: Roman Lazuka <kmb23test@mail.ru>

---
+++ src/core/DomListener.js
@@ -0,0 +1,39 @@
+import {capitalize} from '@core/utils';
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error(`No $root provider for DomListener`)
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        throw new Error(`
+        Method ${method} is not implemented in ${this.name} Component
+        `)
+      }
+      this[method] = this[method].bind(this)
+      // тоже самое что и addEventListener
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      // тоже самое что и removeEventListener
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+
+// input => onInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
+}
